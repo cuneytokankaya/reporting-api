412e981d2ca4e0e0ce60e213a22c786cb12b8831
Cüneyt Okan KAYA <cuneyt.okan.kaya@hotmail.com>
2024-02-05 23:33:23 +0300

    initial commit
    initial commit

---
+++ src/test/java/com/cuneytokankaya/homework/reportingapi/ReportingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuneytokankaya.homework.reportingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
