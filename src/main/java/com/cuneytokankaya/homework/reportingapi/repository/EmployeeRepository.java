f56e58f3e67745c4d8c9ed6fd6cd22cbfc50150d
Cuneyt Okan KAYA <cuneyt.okan.kaya@hotmail.com>
2024-02-13 02:55:48 +0300

    reporting-api dev

--- src/main/java/com/cuneytokankaya/homework/reportingapi/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cuneytokankaya.homework.reportingapi.repository;
-
-import com.cuneytokankaya.homework.reportingapi.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
